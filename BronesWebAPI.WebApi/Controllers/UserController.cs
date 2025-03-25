fdaa2afec9261f418d349242ca76d02e0d93674f
FB99FIRE <felix99@hotmail.nl>
2025-03-26 00:20:36 +0100

    UserController and repository

---
+++ BronesWebAPI.WebApi/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using BronesWebAPI.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BronesWebAPI.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        //private readonly IAuthenticationService _authenticationService;
+        private readonly ILogger<UserController> _logger;
+
+
+        public UserController(IUserRepository userRepository, IAuthenticationService authenticationService, ILogger<UserController> logger) 
+        {
+            _userRepository = userRepository;
+            //_authenticationService = authenticationService;
+            _logger = logger;
+        }
+
+        [HttpGet("{id:guid}",Name = "GetUser")]
+        public async Task<IEnumerable<Models.Users>> Get(Guid UserId)
+        {
+            return await _userRepository.GetById(UserId);
+  
+        }
+
+        [HttpPost(Name = "AddUser")]
+        public Models.Users Post([FromBody] Models.Users user)
+        {
+            user.UserId = Guid.NewGuid();
+            _userRepository.Add(user);
+            return user;
+        }
+
+        [HttpDelete("{id:guid}", Name = "DeleteUser")]
+        public async Task<IActionResult> Delete(Guid UserId)
+        {
+            var user = await _userRepository.GetById(UserId);
+            if (user == null)
+            {
+                return NotFound(new { message = "Environment not found" });
+            }
+
+            await _userRepository.DeleteAsync(UserId);
+            return NoContent();
+        }
+
+        //we gaan geen users hoeven updaten maar hier heb je m voor nu
+        [HttpPut("{id:Guid}", Name = "UpdateUser")]
+        public async Task<IActionResult> Put(Guid UserId, [FromBody] Models.Users updatedUser)
+        {
+            var user = await _userRepository.GetById(UserId);
+            if (user == null)
+            {
+                return NotFound(new { message = "Environment not found" });
+            }
+
+            // Use async update method
+            await _userRepository.UpdateAsync(UserId, updatedUser);
+            return Ok(updatedUser);
+        }
+    }
+}
---
+++ BronesWebAPI.WebApi/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using BronesWebAPI.WebApi.Models;
+
+namespace BronesWebAPI.WebApi.Repositories
+{
+    public interface IUserRepository
+    {
+        Task Add(Users users);
+        Task Delete(Guid UserId);
+        Task DeleteAsync(Guid UserId);
+        Task<IEnumerable<Users>> GetAll();
+        Task<IEnumerable<Users?>> GetById(Guid UserId);
+        Task InsertAsync(Guid UserId, string Name, DateOnly DateOfBirth, string Behandelplan, string NaamArts, DateOnly EersteAfspraak);
+        Task<IEnumerable<Users>> ReadAsync();
+        Task Update(Guid UserId, Users updatedUser);
+        Task UpdateAsync(Guid UserId, Users updatedUsers);
+    }
+}
