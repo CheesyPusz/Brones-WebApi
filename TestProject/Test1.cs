c5902684d36f65b7c2c5e210291ca5d2301f6799
Youp van Haagen <yhj.vanhaagen@student.avans.nl>
2025-03-31 13:43:32 +0200

    Test van een patiënt toevoegen en verwijderen

---
+++ TestProject/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProject
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void Test_of_testen_werkt()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
